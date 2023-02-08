752c91611b89b7f1e20655850aeb0f5f588db664
Renaud Kyoku <676896+rkyoku@users.noreply.github.com>
2023-02-08 18:52:37 +0100

    php 8.2

--- lib/PayPalCheckoutSdk/Core/PayPalHttpClient.php
+++ lib/PayPalCheckoutSdk/Core/PayPalHttpClient.php
@@ -8,6 +8,7 @@ class PayPalHttpClient extends HttpClient
 {
     private $refreshToken;
     public $authInjector;
+    public mixed $curlCls = null;
 
     public function __construct(PayPalEnvironment $environment, $refreshToken = NULL)
     {
